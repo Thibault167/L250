7860e73530d92d086b41a31fb91c87326068ae33
Thibault Dufour <thibault.dufour@etu.unilim.fr>
2023-04-05 14:12:04 +0200

    first component and first route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
